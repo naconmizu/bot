153ebb62a6bc2d1868cf99e6107cf569b7b6921a
naconmizu <mctangerina@gmail.com>
2025-11-15 21:10:03 -0300

    import and database Env bugs fixed

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB conectado!");
   } catch (err) {
     console.error("Erro ao conectar no MongoDB:", err);
